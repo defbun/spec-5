37211b89397359fb38356f05bf259accc3ad80c6
defbun <147871947+defbun@users.noreply.github.com>
2025-04-25 16:50:40 +0300

    Add files via upload

---
+++ m.js
@@ -0,0 +1,4 @@
+var w=window,d=document;
+
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(w,d,'script','https://mc.yandex.ru/metrika/tag.js','ym');
+ym(105277,'init',{clickmap:true,trackLinks:true,accurateTrackBounce:true});
